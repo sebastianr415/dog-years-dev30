ab69cdfe5723c98d9a4913210cf6e78b40cc75c7
Sebastian Ramirez <85366730+sebastianr415@users.noreply.github.com>
2024-07-26 06:58:17 -0700

    Create dog.js

---
+++ dog.js
@@ -0,0 +1,23 @@
+//How old Am If I was a dog
+//created an variable called myAge and asssigned it to my current age
+let myAge = 25;
+
+//assigning a variable to the number 2 
+let earlyYears = 2
+//multiplying varible my an interger than reassigning it to a variable
+earlyYears *= 10.5
+// console.log(earlyYears)
+
+//acounting the first two years of a dogs life into my age
+laterYears = myAge - 2
+//calculating the numbr of dog years accoounted for by my later years
+laterYears *= 4
+//storing two varibles into a variable
+let myAgeInDogYears = earlyYears + laterYears
+
+let myName = 'sebastian'.toLowerCase()
+console.log(earlyYears)
+console.log(laterYears)
+
+//loging into the console using interpolation with results of my variables
+console.log(`my name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`)
